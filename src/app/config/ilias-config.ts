9ce40605ee6a23bddadf3739b9c79bbc389e6d85
Jakub Niewelt <jakubniewelt@icloud.com>
2024-05-01 08:41:59 +0200

    feat: change app name to IACUBUS

--- src/app/config/ilias-config.ts
+++ src/app/config/ilias-config.ts
@@ -1,27 +1,27 @@
 /** angular */
-import {Injectable, InjectionToken} from "@angular/core";
+import { Injectable, InjectionToken } from "@angular/core";
 import { TranslateService } from "@ngx-translate/core";
 import { AuthenticationProvider } from "../providers/authentication.provider";
 /** misc */
-import {HttpClient, HttpResponse} from "../providers/http";
-import {isDefined} from "../util/util.function";
+import { HttpClient, HttpResponse } from "../providers/http";
+import { isDefined } from "../util/util.function";
 import { Optional } from "../util/util.optional";
 
 const CONFIG_FILE: string = "./assets/config.json";
 
 export interface ILIASConfig {
-  readonly installations: Array<ILIASInstallation>
+    readonly installations: Array<ILIASInstallation>;
 }
 
 export interface ILIASInstallation {
-  readonly id: number;
-  readonly title: string;
-  readonly url: string;
-  readonly clientId: string;
-  readonly apiKey: string;
-  readonly apiSecret: string;
-  readonly accessTokenTTL: number;
-  readonly privacyPolicy: string;
+    readonly id: number;
+    readonly title: string;
+    readonly url: string;
+    readonly clientId: string;
+    readonly apiKey: string;
+    readonly apiSecret: string;
+    readonly accessTokenTTL: number;
+    readonly privacyPolicy: string;
 }
 
 export const CONFIG_PROVIDER: InjectionToken<ConfigProvider> = new InjectionToken<ConfigProvider>("Token for ConfigProvider");
@@ -33,31 +33,30 @@ export const CONFIG_PROVIDER: InjectionToken<ConfigProvider> = new InjectionToke
  * @version 1.0.0
  */
 export interface ConfigProvider {
+    /**
+     * Loads the config file.
+     *
+     * @returns {Promise<ILIASConfig>} the resulting
+     */
+    loadConfig(): Promise<ILIASConfig>;
 
-  /**
-   * Loads the config file.
-   *
-   * @returns {Promise<ILIASConfig>} the resulting
-   */
-  loadConfig(): Promise<ILIASConfig>
+    /**
+     * Loads the config file and searches for an installation
+     * matching the given {@code installationId}.
+     *
+     * @param {number} installationId unique identifier of the installation
+     *
+     * @returns {Promise<ILIASInstallation>} the resulting ILIAS installation
+     * @throw {ReferenceError} if the given id does not exists
+     */
+    loadInstallation(installationId: number): Promise<Optional<ILIASInstallation>>;
 
-  /**
-   * Loads the config file and searches for an installation
-   * matching the given {@code installationId}.
-   *
-   * @param {number} installationId unique identifier of the installation
-   *
-   * @returns {Promise<ILIASInstallation>} the resulting ILIAS installation
-   * @throw {ReferenceError} if the given id does not exists
-   */
-  loadInstallation(installationId: number): Promise<Optional<ILIASInstallation>>
-
-  /**
-   * Returns the last loaded installation
-   *
-   * @returns {Promise<ILIASInstallation>} the resulting ILIAS installation
-   */
-  getInstallation(): Promise<Optional<Readonly<ILIASInstallation>>>
+    /**
+     * Returns the last loaded installation
+     *
+     * @returns {Promise<ILIASInstallation>} the resulting ILIAS installation
+     */
+    getInstallation(): Promise<Optional<Readonly<ILIASInstallation>>>;
 }
 
 /**
@@ -68,23 +67,18 @@ export interface ConfigProvider {
  * @version 1.0.0
  */
 @Injectable()
- export class ILIASConfigProvider implements ConfigProvider {
-
+export class ILIASConfigProvider implements ConfigProvider {
     private readonly config: Promise<ILIASConfig>;
     private installation: Promise<Optional<ILIASInstallation>> = Promise.resolve(Optional.empty());
 
-    constructor(
-        private readonly http: HttpClient,
-        private readonly translate: TranslateService
-    ) {
+    constructor(private readonly http: HttpClient, private readonly translate: TranslateService) {
         this.config = this.loadFile();
         this.observeTranslation();
     }
 
     observeTranslation(): void {
         this.translate.onLangChange.subscribe(async (lang: string) => {
-            if (AuthenticationProvider.isLoggedIn())
-                await this.loadInstallation(AuthenticationProvider.getUser().installationId);
+            if (AuthenticationProvider.isLoggedIn()) await this.loadInstallation(AuthenticationProvider.getUser().installationId);
         });
     }
 
@@ -92,26 +86,24 @@ export interface ConfigProvider {
         return this.installation;
     }
 
-    async loadConfig(): Promise<ILIASConfig> { return this.config }
+    async loadConfig(): Promise<ILIASConfig> {
+        return this.config;
+    }
 
     async loadInstallation(installationId: number): Promise<Optional<Readonly<ILIASInstallation>>> {
-
         const iliasConfig: ILIASConfig = await this.config;
 
-        let installation: ILIASInstallation | undefined = iliasConfig.installations
-        .find(it => it.id == installationId);
+        let installation: ILIASInstallation | undefined = iliasConfig.installations.find((it) => it.id == installationId);
 
         installation = {
-        id: installation.id,
-        title: installation.title,
-        url: installation.url,
-        clientId: installation.clientId,
-        apiKey: installation.apiKey,
-        apiSecret: installation.apiSecret,
-        accessTokenTTL: installation.accessTokenTTL,
-        privacyPolicy: installation.privacyPolicy
-            ? installation.privacyPolicy
-            : `https://www.ilias-pegasus.de/${await this.translate.get("privacy").toPromise()}/`
+            id: installation.id,
+            title: installation.title,
+            url: installation.url,
+            clientId: installation.clientId,
+            apiKey: installation.apiKey,
+            apiSecret: installation.apiSecret,
+            accessTokenTTL: installation.accessTokenTTL,
+            privacyPolicy: installation.privacyPolicy ? installation.privacyPolicy : `https://deepportal.hq.nato.int/eacademy/${await this.translate.get("privacy").toPromise()}/`,
         };
 
         if (isDefined(installation)) {
@@ -122,40 +114,40 @@ export interface ConfigProvider {
         throw new ReferenceError(`Installation with id '${installationId}' does not exists in file: ${CONFIG_FILE}`);
     }
 
-  private async loadFile(): Promise<ILIASConfig> {
-    const response: HttpResponse = await this.http.get(CONFIG_FILE);
+    private async loadFile(): Promise<ILIASConfig> {
+        const response: HttpResponse = await this.http.get(CONFIG_FILE);
 
-    return response.json<ILIASConfig>(configSchema);
-  }
+        return response.json<ILIASConfig>(configSchema);
+    }
 }
 
 const configSchema: object = {
-  "title": "config",
-  "type": "object",
-  "properties": {
-    "installations": {
-      "type": "array",
-      "items:": {
-        "type": "object",
-        "properties": {
-          "id": {
-            "type": "number",
-            "min": 1
-          },
-          "title": {"type": "string"},
-          "url": {"type": "string"},
-          "clientId": {"type": "string"},
-          "apiKey": {"type": "string"},
-          "apiSecret": {"type": "string"},
-          "accessTokenTTL": {"type": "number"},
-          "privacyPolicy": {
-            "type": "string",
-            "default": "https://www.ilias-pegasus.de/datenschutz/"
-          }
+    title: "config",
+    type: "object",
+    properties: {
+        installations: {
+            type: "array",
+            "items:": {
+                type: "object",
+                properties: {
+                    id: {
+                        type: "number",
+                        min: 1,
+                    },
+                    title: { type: "string" },
+                    url: { type: "string" },
+                    clientId: { type: "string" },
+                    apiKey: { type: "string" },
+                    apiSecret: { type: "string" },
+                    accessTokenTTL: { type: "number" },
+                    privacyPolicy: {
+                        type: "string",
+                        default: "https://deepportal.hq.nato.int/eacademy/iacobus-privacy-policy/",
+                    },
+                },
+                required: ["id", "title", "url", "clientId", "apiKey", "apiSecret", "accessTokenTTL"],
+            },
         },
-        "required": ["id", "title", "url", "clientId", "apiKey", "apiSecret", "accessTokenTTL"]
-      }
-    }
-  },
-  "required": ["installations"]
+    },
+    required: ["installations"],
 };
--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,24 @@
-import {enableProdMode} from "@angular/core";
-import {platformBrowserDynamic} from "@angular/platform-browser-dynamic";
-import {AppModule} from "./app/app.module";
-import {environment} from "./environments/environment";
-import {useStandard} from "./standard";
+import { enableProdMode } from "@angular/core";
+import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
+import { AppModule } from "./app/app.module";
+import { environment } from "./environments/environment";
+import { useStandard } from "./standard";
 // Required for typeorm library
 // tslint:disable-next-line:no-import-side-effect
 import "reflect-metadata";
 
 if (window.isSecureContext !== true) {
-    const errorMessage: string = "The ILIAS Pegasus App needs a secure context! " +
+    const errorMessage: string =
+        "The IACUBUS App needs a secure context! " +
         "Make sure this app is served over https://, file:// or http://localhost";
     throw new Error(errorMessage);
 }
 
 if (environment.production) {
-  enableProdMode();
+    enableProdMode();
 }
 
 useStandard();
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+platformBrowserDynamic()
+    .bootstrapModule(AppModule)
+    .catch((err) => console.error(err));
